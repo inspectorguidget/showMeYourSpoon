b3ddc05174bbf4bc1944456b72a5a29c99fd4d7d
arnobl <1052622+arnobl@users.noreply.github.com>
2019-07-29 15:47:29 +0200

    clean

--- src/main/java/showmeyourspoon/ShowMe.java
+++ src/main/java/showmeyourspoon/ShowMe.java
@@ -1,24 +1,14 @@
 package showmeyourspoon;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import showmeyourspoon.spoon.BasicVisitor;
-import showmeyourspoon.spoon.ConsolePrinter;
-import spoon.Launcher;
-import spoon.reflect.declaration.CtClass;
 
 public class ShowMe extends Application {
 	public static void main(final String[] args) {
 		launch(args);
-
-		if(args.length > 1) {
-			final CtClass<?> elt = Launcher.parseClass(args[1]);
-			elt.accept(new BasicVisitor(new ConsolePrinter(), true));
-		}
 	}
 
 	@Override
